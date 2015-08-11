8104451c8bd9d954f2b8ef22fcd3f2a55f8b9874
simonBeltram <simon.beltra@gmail.com>
2015-08-11 12:17:12 +0200

    loading web page - done

--- src/PitayaServer.java
+++ src/PitayaServer.java
@@ -12,7 +12,7 @@ public class PitayaServer implements Runnable {
 	private String token = "";
 	private int pitayaNum = -1;
 	private int user = -1;
-	private String fileName="";
+	private String fileName,method,contentType="";
 	private String data="";
 	private final int BUFFER_SIZE= 1024;
 	
@@ -25,36 +25,39 @@ public class PitayaServer implements Runnable {
 	@Override
 	public void run() {
 
-		openServerSocket();
-		byte[] buffer= new byte[BUFFER_SIZE];
+		openServerSocket();      				//init the server socket
+		byte[] buffer= new byte[BUFFER_SIZE];	//init the buffer to be used later for reading from input
 
 		while (this.running) {
 			try {
-				Socket client = this.server.accept();
+				Socket client = this.server.accept(); //accept incoming connections
 				if (client.isConnected()) {
 					System.out.println("Client connected!\n IP:"
 							+ client.getInetAddress() + "\n Port: "
 							+ client.getPort());
 					
-				InputStream input = client.getInputStream();
+				InputStream input = client.getInputStream();     //open input/output streams
 				OutputStream output = client.getOutputStream();
 				String header="";
 				
-				input.read(buffer);
-				data = new String(buffer, "UTF-8");
+				input.read(buffer);					//read the http request
+				data = new String(buffer, "UTF-8"); 
 				
-					if (data.startsWith("GET") && getParameters(data)){
+					if (data.startsWith("GET") && getParameters(data)){  //If we have a http GET request (user loading web page)
 						
-						File f = new File("apps/fileName");
-						if (f.exists()){
+						File f = new File("apps/"+fileName);			
+						if (f.exists()){       							//Check if we have the specified file
 
-							header ="HTTP/1.1 200 OK\r\n"
-									+"Content-type: text/html; charset=UTF-8\r\n";
+							header ="HTTP/1.1 200 OK\r\n" 				
+									+"Content-type:contentType\r\n";
+									
 							long fileSize = f.length();
 							
 								header+="Content-size: "+fileSize+"\r\n";
+								header+="Connection: Close\r\n\r\n";     //Http response status line and header
 							
-							FileInputStream fis = new FileInputStream(f);
+							FileInputStream fis = new FileInputStream(f); //reading from file (http body) and sending (with http header) 
+							output.write(header.getBytes());
 							int ch = fis.read(buffer, 0, BUFFER_SIZE);
 							while (ch != -1) 
 							{
@@ -65,17 +68,19 @@ public class PitayaServer implements Runnable {
 						}else{
 							header = "HTTP/1.1 404 Not found\r\n";
 						}
+									
 						
 						
-						
-					}else if (data.startsWith("updateData")){
+					}else if (data.startsWith("updateData")){           //if we receive a string updateData, we have a socket conn. (web page already loaded)
 						
 					}else{
 						input.close();
 						client.close();
 					}
-
+					output.close();
+					client.close();
 				}
+				
 
 			} catch (IOException e) {
 				if (!running && server.isClosed())
@@ -86,6 +91,7 @@ public class PitayaServer implements Runnable {
 					}
 
 			}
+			
 		}
 		System.out.println("Service stoped");
 
@@ -117,19 +123,30 @@ public class PitayaServer implements Runnable {
 	}
 	public boolean getParameters(String data) {
 		try {
-			System.out.println(data);
-			String params[]= data.split("&");
-			fileName= data.split(" ")[1].substring(0,data.indexOf("/"));
+			String[] params=null; 
+			String[] lines = data.split("\r\n");			//Split entire package into lines
+			String[] status= data.split(" ");				//Split the status line in three parts (0-method, 1-URL, 2-protocol version)
+			method = status[0];
 			
-			if (params.length>1){
+			if (status[1].indexOf("&")>0){					//Check if we have any GET parameters
+				String[] resource = status[1].split("\\?"); //Separate the URL from the parameters
+				fileName = resource[0];							
+				if (resource.length > 1){ 					
+					 params= resource[1].split("&");		//Divide each parameter
+				}
+			}else{											//If no parameter is present, take the entire string
+				fileName=status[1];
+			}
+			if (params != null){							//
 				this.pitayaNum=Integer.parseInt(params[0].split("=")[1]);
 				this.token=params[1].split("=")[1];
-			}else
-				this.token=params[0].split("=")[1];
+			}
 			
-			System.out.println("fileName: "+fileName+" pitayaNum: "+pitayaNum+" token: "+pitayaNum);
+			contentType=lines[3].split(":")[1];             //Take the content type from the http request
+			if (contentType.startsWith("text/html")) contentType = "text/html"; //hack: for html pages use simpler content type string
 
-			if (pitayaNum > Main.lookupTable.length){
+		
+			if (pitayaNum > Main.lookupTable.length){       //Check if pitayaNum is a valid number
 				return false;
 			}
 			
