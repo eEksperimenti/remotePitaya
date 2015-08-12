b40ec1cfd228827ad9658b6a2e625f37039f9c73
simonBeltram <simon.beltra@gmail.com>
2015-08-12 15:07:48 +0200

    data fetching from pitaya (not done yet)-2

--- src/PitayaDataFetcher.java
+++ src/PitayaDataFetcher.java
@@ -13,42 +13,43 @@ public class PitayaDataFetcher implements Runnable{
 	}
 	public void run() {
 		try {
-			Thread.sleep(50);
-		URL appUrl = new URL("http://"+this.ip+":80/soncna_celica");
-		HttpURLConnection tmpConn = (HttpURLConnection) appUrl.openConnection();
-		tmpConn.setRequestMethod("GET");
-		tmpConn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-		tmpConn.setRequestProperty("Referer:","http://192.168.94.134/apps/");
-		tmpConn.setRequestProperty("Connection", "keep-alive");
-		tmpConn.connect();
+			URL appUrl = new URL("http://"+this.ip+":80/soncna_celica");
+			HttpURLConnection tmpConn = (HttpURLConnection) appUrl.openConnection();
+			tmpConn.setRequestMethod("GET");
+			tmpConn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+			tmpConn.setRequestProperty("Referer:","http://192.168.94.134/apps/");
+			tmpConn.setRequestProperty("Connection", "keep-alive");
+			tmpConn.connect();
 		
-		URL url = new URL("http://"+this.ip+":80/data");
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Content-type", "application/json;charset=utf-8");
-		conn.setRequestProperty("X-Requested-With","XMLHttpRequest");
-		conn.setRequestProperty("Connection", "keep-alive");
-
-		conn.connect();
-		System.out.println("Data fetching from Pitaya: "+ip);
-
-		BufferedReader br=null;
+			while (true){
+				Thread.sleep(50);
+	
+				URL url = new URL("http://"+this.ip+":80/data?_=1437573753404");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Content-type", "application/json;charset=utf-8");
+				conn.setRequestProperty("X-Requested-With","XMLHttpRequest");
+				conn.setRequestProperty("Connection", "keep-alive");
+				conn.connect();
+				System.out.println("Data fetching from Pitaya: "+ip);
 		
-		if (conn.getResponseCode() == 200 || conn.getResponseCode() == 201){
-			br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String jsonData="",tmp="";
-			while ((tmp = br.readLine()) != null){
-				jsonData +=tmp;
-			}
-			System.out.println("IP: "+ip+" DATA:\n"+jsonData);
-			//add to circural buffer
+				BufferedReader br=null;
+				
+				if (conn.getResponseCode() == 200 || conn.getResponseCode() == 201){
+					br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+					String jsonData="",tmp="";
+					while ((tmp = br.readLine()) != null){
+						jsonData +=tmp;
+					}
+					System.out.println("IP: "+ip+" DATA:\n"+jsonData);
+					//add to circural buffer
+							
+					jsonData="";
 					
-			jsonData="";
-			
+				}
+				br.close();
+				conn.disconnect();
 		}
-		br.close();
-		conn.disconnect();
-		
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -58,7 +59,8 @@ public class PitayaDataFetcher implements Runnable{
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}	
+			
 	}
-
+	
 }
